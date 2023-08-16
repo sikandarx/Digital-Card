9a9e5756ae57bb1ed1e5e95f92e90aa55337de28
Sikandar Habib <105446874+sikandarx@users.noreply.github.com>
2023-08-16 14:35:43 +0500

    Update Interests.jsx

--- New digital-card/src/Interests.jsx
+++ New digital-card/src/Interests.jsx
@@ -4,7 +4,8 @@ function Interests(){
     return(
         <>
         <div className='heading mt-3'>Interests</div>
-        <div className='para'>Food expert. Music scholar. Reader. Internet fanatic. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</div>
+        <div className='para'>Savvy gamer. Internet fanatic. Tech Aficionado. Entrepreneur. Pixel Perfectionist. 
+        Coffee Maestro. Aesthetic Visionary.</div>
         </>
     )
 }
